a088ce4ca2f7c044fcaf356da1a8b032ac1c165f
adriansuarez1999 <feradrian1999@gmail.com>
2025-09-11 23:55:08 -0300

    se agrega modelos para los post (Category, Post, Comment, PostImage)

--- blog/apps/post/models.py
+++ blog/apps/post/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
+from django.utils.text import slugify
+import uuid
+import os
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=200, blank=True)
+    content = models.TextField(max_length=10000)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="post"
+    )
+    create_at = models.DateTimeField(default=timezone.now)
+    update_at = models.DateTimeField(auto_now=True)
+    allow_comments = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def amount_comments(self):
+        return self.comments.count()
+
+    def generate_unique_slug(self):
+        slug = slugify(self.title)
+        unique_slug = slug
+        num = 1
+
+        while Post.objects.filter(slug=unique_slug).exists():
+            unique_slug = f"{slug}-{num}"
+            num += 1
+
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.generate_unique_slug()
+        super().save(*args, **kwargs)
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField(max_length=400)
+    creted_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
+
+
+def get_image_path(instance, filename):
+    post_id = instance.post.id
+    images_count = instance.post.images.count()
+    _, file_extension = os.path.splitext(filename)
+    new_filename = f"post_{post_id}_image_{images_count + 1}{file_extension}"
+
+    return os.path.join("post/cover/", new_filename)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="images")
+    image = models.ImageField(upload_to=get_image_path)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"PostImage {self.id}"
