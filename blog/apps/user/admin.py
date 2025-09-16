bb72c85e3ba2158ba1a7d40efc003de84c351881
adriansuarez1999 <feradrian1999@gmail.com>
2025-09-16 13:12:22 -0300

    se agregan funcionalidades en admin.py de post y user

--- blog/apps/user/admin.py
+++ blog/apps/user/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from apps.user.models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("alias", "avatar")}),)
+
+    add_fieldsets = (
+        (None, {"fields": ("username", "email", "avatar", "password1", "password2")}),
+    )
+
+    def is_registered(self, obj):
+        return obj.groups.filter(name="Registered").exists()
+
+    is_registered.short_description = "Es usuario registrado"
+    is_registered.boolean = True
+
+    def is_collaborator(self, obj):
+        return obj.groups.filter(name="Collaborator").exists()
+
+    is_collaborator.short_description = "Es usuario colaborador"
+    is_collaborator.boolean = True
+
+    def is_admin(self, obj):
+        return obj.groups.filter(name="Admin").exists()
+
+    is_admin.short_description = "Es usuario administrador"
+    is_admin.boolean = True
+
+
+admin.site.register(User, CustomUserAdmin)
