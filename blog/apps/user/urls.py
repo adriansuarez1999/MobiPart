87602931f8b1b609d0ce95f317697af2e714b758
adriansuarez1999 <feradrian1999@gmail.com>
2025-10-07 09:16:26 -0300

    feat: Implement user authentication forms and views
    - Added LoginForm and updated RegisterForm in user/forms.py.
    - Created LoginView and LogoutView in user/views.py for handling user login and logout.
    - Updated URLs in user/urls.py to include login and logout paths.
    - Created auth_login.html template for user login interface.
    - Updated auth_register.html to support file uploads for avatar.
    - Enhanced footer and header components with new styles and functionality.
    - Added category filters in category_filters.html for better product navigation.
    - Implemented JavaScript for dropdown menus and add-to-cart functionality.
    - Introduced a new CSS file for styling the application.
    - Updated base_layout.html to include Font Awesome and custom styles.

--- blog/apps/user/urls.py
+++ blog/apps/user/urls.py
@@ -6,5 +6,6 @@ app_name = "user"
 urlpatterns = [
     path("user/profile", views.UserProfileView.as_view(), name="user_profile"),
     path("auth/register", views.RegisterView.as_view(), name="auth_register"),
-    # path('auth/login', views.LoginView.as_view(), name='auth_login'),
+    path("auth/login", views.LoginView.as_view(), name="auth_login"),
+    path("auth/logout", views.LogoutView.as_view(), name="auth_logout"),
 ]
