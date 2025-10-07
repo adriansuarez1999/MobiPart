27df17b7f8c30fb5e9c6e7b856c5d54f02187f93
adriansuarez1999 <feradrian1999@gmail.com>
2025-10-07 13:04:55 -0300

    feat: Add post filtering form and pagination to post list view

--- blog/apps/post/urls.py
+++ blog/apps/post/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path  
-from apps.post import views as views 
+from django.urls import path
+from apps.post import views as views
 
-app_name = 'post'
+app_name = "post"
 
 urlpatterns = [
-    path('post/<slug:slug>', views.PostDetailView.as_view(), name='post_detail'),
+    path("post/", views.PostListView.as_view(), name="post_list"),
+    path("post/<slug:slug>", views.PostDetailView.as_view(), name="post_detail"),
 ]
--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,5 +1,62 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from apps.post.models import *
+from apps.post.forms import PostFilterForm
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post/post_list.html"
+    context_object_name = "posts"
+
+    paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["filter_form"] = PostFilterForm(self.request.GET)
+
+        if context.get("is_paginated", False):
+            query_params = self.request.GET.copy()
+            query_params.pop("page", None)
+
+            pagination = {}
+            page_obj = context["page_obj"]
+            paginator = context["paginator"]
+
+            if page_obj.number > 1:
+                pagination["first_page"] = (
+                    f"?{query_params.urlencode()}&page={paginator.page_range[0]}"
+                )
+
+            if page_obj.has_previous():
+                pagination["previous_page"] = (
+                    f"?{query_params.urlencode()}&page={page_obj.number -1}"
+                )
+
+            if page_obj.has_next():
+                pagination["next_page"] = (
+                    f"?{query_params.urlencode()}&page={page_obj.number +1}"
+                )
+
+            if page_obj.number < paginator.num_pages:
+                pagination["last_page"] = (
+                    f"?{query_params.urlencode()}&page={paginator.num_pages}"
+                )
+            context["pagination"] = pagination
+
+        return context
 
 
 class PostDetailView(TemplateView):
+    template_name = "post/post_detail.html"
+
+
+class PostCreateView(TemplateView):
+    template_name = "post/post_detail.html"
+
+
+class PostUpdateView(TemplateView):
+    template_name = "post/post_detail.html"
+
+
+class PostDeleteView(TemplateView):
     template_name = "post/post_detail.html"
