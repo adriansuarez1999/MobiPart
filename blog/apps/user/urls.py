e0af72fed5140be1a6221c17ba5a533c8e17b53f
Lautaro Orellana <feradrian1999@gmail.com>
2025-10-26 14:06:09 -0300

    se agregan mas funcionalidades

--- blog/apps/user/urls.py
+++ blog/apps/user/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
     path("auth/register", views.RegisterView.as_view(), name="auth_register"),
     path("auth/login", views.LoginView.as_view(), name="auth_login"),
     path("auth/logout", views.LogoutView.as_view(), name="auth_logout"),
+]
