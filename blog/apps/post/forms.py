cd81cbaac5b261d68cf1c773cc9861f4a53530e3
adriansuarez1999 <131199588+adriansuarez1999@users.noreply.github.com>
2025-10-28 12:11:21 -0300

    User register (#17)
    * se agregan funcionalidades para el registro de nuevos usuarios
    
    * feat: Implement user authentication forms and views
    
    - Added LoginForm and updated RegisterForm in user/forms.py.
    - Created LoginView and LogoutView in user/views.py for handling user login and logout.
    - Updated URLs in user/urls.py to include login and logout paths.
    - Created auth_login.html template for user login interface.
    - Updated auth_register.html to support file uploads for avatar.
    - Enhanced footer and header components with new styles and functionality.
    - Added category filters in category_filters.html for better product navigation.
    - Implemented JavaScript for dropdown menus and add-to-cart functionality.
    - Introduced a new CSS file for styling the application.
    - Updated base_layout.html to include Font Awesome and custom styles.
    
    * feat: Add post filtering form and pagination to post list view
    
    * se agregan funcionalidades

--- blog/apps/post/forms.py
+++ blog/apps/post/forms.py
@@ -4,16 +4,11 @@ from django import forms
 class PostFilterForm(forms.Form):
     search_query = forms.CharField(
         required=False,
-        widget=forms.TextInput(
-            attrs={"placeholder": "Buscar...", "class": "w-full p-2"}
-        ),
     )
     order_by = forms.ChoiceField(
         required=False,
         choices=(
-            ("-created_at", "Más recientes"),
-            ("created_at", "Más antiguos"),
-            ("-comments_count", "Más comentados"),
+            ("-create_at", "Más recientes"),
+            ("create_at", "Más antiguos"),
         ),
-        widget=forms.Select(attrs={"class": "w-full p-2"}),
     )
