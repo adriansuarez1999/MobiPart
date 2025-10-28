509e9efd1e60e32b03a6663e20fe606d30d22191
adriansuarez1999 <feradrian1999@gmail.com>
2025-10-28 11:51:38 -0300

    se agregan funcionalidades

--- blog/apps/post/views.py
+++ blog/apps/post/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView, ListView
+from django.db.models import Count, Q
 from apps.post.models import *
 from apps.post.forms import PostFilterForm
+from django_filters.views import FilterView
+from apps.post.filters import PostFilter
 
 
 class PostListView(ListView):
@@ -8,11 +11,26 @@ class PostListView(ListView):
     template_name = "post/post_list.html"
     context_object_name = "posts"
 
-    paginate_by = 1
+    paginate_by = 8
+
+    def get_queryset(self):
+        queryset = Post.objects.all().annotate(comments_count=Count("comments"))
+        search_query = self.request.GET.get("search_query", "")
+        order_by = self.request.GET.get("order_by", "-create_at")
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query)
+                | Q(author__username__icontains=search_query)
+                | Q(content__icontains=search_query)
+                | Q(category__name__icontains=search_query)
+            )
+        return queryset.order_by(order_by)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["filter_form"] = PostFilterForm(self.request.GET)
+        context["categories"] = Category.objects.all()
 
         if context.get("is_paginated", False):
             query_params = self.request.GET.copy()
@@ -60,3 +78,13 @@ class PostUpdateView(TemplateView):
 
 class PostDeleteView(TemplateView):
     template_name = "post/post_detail.html"
+
+
+# FILTROS
+
+
+# filtro para buscador
+class PostFilterView(FilterView):
+    model = Post
+    filterset_class = PostFilter
+    template_name = "post/post_list.html"
