d71a7895b5acbc9c9da6945434bd953d69717e9e
adriansuarez1999 <feradrian1999@gmail.com>
2025-11-18 11:50:44 -0300

    mejora el diseño y la estructura del perfil de usuario, optimizando estilos y organización del código HTML

--- blog/apps/user/forms.py
+++ blog/apps/user/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.models import User
-from apps.user.models import User
+from django.contrib.auth import get_user_model
 from django import forms
+from apps.user.models import User
 
 
 class RegisterForm(UserCreationForm):
@@ -14,12 +14,32 @@ class LoginForm(AuthenticationForm):
     username = forms.CharField(
         max_length=150,
         widget=forms.TextInput(
-            attrs={"class": "form-control", "paceholder": "Usuario"}
+            attrs={"class": "form-control", "placeholder": "Usuario"}
         ),
     )
     password = forms.CharField(
-        widget=forms.TextInput(
-            attrs={"class": "form-control", "paceholder": "Contraseña"}
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Contraseña"}
         ),
     )
 
+
+User = get_user_model()
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "alias", "avatar"]
+        widgets = {
+            "username": forms.TextInput(
+                attrs={"class": "input-perfil", "id": "id_username"}
+            ),
+            "email": forms.EmailInput(
+                attrs={"class": "input-perfil", "id": "id_email"}
+            ),
+            "alias": forms.TextInput(attrs={"class": "input-perfil", "id": "id_alias"}),
+            "avatar": forms.ClearableFileInput(
+                attrs={"class": "input-perfil", "id": "id_avatar", "accept": "image/*"}
+            ),
+        }
